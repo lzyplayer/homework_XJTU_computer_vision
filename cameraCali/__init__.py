daae9b45d2d59cda5ef0383aa2e84bfe0258197a
lzyplayer <292442545@qq.com>
2019-01-20 21:51:10 +0800

    work updated

--- cameraCali/__init__.py
+++ cameraCali/__init__.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     seleimg='../shots/02.jpg'
     obj_p, img_p, img_shape = getCorner('../shots/*.jpg', 6, 8)
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(obj_p, img_p, img_shape, None, None)
+    print("camera matrix\n", mtx)
+    print("distortion coefficients\n", dist)
     img = cv.imread(seleimg)
     h, w = img.shape[:2]
     newmtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0, (w, h)) #优化变换矩阵，给出兴趣区域
